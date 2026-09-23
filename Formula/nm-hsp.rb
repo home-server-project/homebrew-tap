371693645c10357e4c194b09778341b16928a090
iegorch86 <66792505+iegorch86@users.noreply.github.com>
2026-09-23 01:18:30 -0400

    Use prebuilt nm-hsp release binaries

--- Formula/nm-hsp.rb
+++ Formula/nm-hsp.rb
@@ -1,16 +1,23 @@
 class NmHsp < Formula
   desc "Friendly terminal interface for NetworkManager"
   homepage "https://github.com/home-server-project/nm-hsp"
-  url "https://github.com/home-server-project/nm-hsp.git",
-      tag:      "v0.2.0",
-      revision: "6431fc7376021edd3278016b1374f1a98dd225ca"
+  version "0.2.0"
   license "Apache-2.0"
 
-  depends_on "go" => :build
+  on_intel do
+    url "https://github.com/home-server-project/nm-hsp/releases/download/v0.2.0/nm-hsp_0.2.0_linux_amd64.tar.gz"
+    sha256 "4342daba973e40a2dc113e2aa703a5e4f4bd7fd5ee1c178226066cbe915cf612"
+  end
+
+  on_arm do
+    url "https://github.com/home-server-project/nm-hsp/releases/download/v0.2.0/nm-hsp_0.2.0_linux_arm64.tar.gz"
+    sha256 "e2fccba380d5d3854d95fe4955fda32fb6c0ef88486798ac9026cf2cc9b3335d"
+  end
+
   depends_on :linux
 
   def install
-    system "go", "build", *std_go_args, "./cmd/nm-hsp"
+    bin.install "nm-hsp"
   end
 
   def caveats
